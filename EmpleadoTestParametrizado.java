59aaaf8d999fef28bb2b9602eda2c5fab67e1eb7
Fernando Repullo <frepullo147@ieszaidinvergeles.org>
2021-03-09 14:21:35 +0100

    Update EmpleadoTestParametrizado.java
    09-03-21 -> Cambios en los tipos de datos.
                    -> Los datos "codEmpleado" y "departamento" ahora son Strings

--- EmpleadoTestParametrizado.java
+++ EmpleadoTestParametrizado.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.*;
 
 import java.lang.reflect.Array;
@@ -12,12 +13,12 @@ import org.junit.runners.Parameterized.Parameters;
 public class EmpleadoTestParametrizado {
 
     private String nombre, oficio;
-    private long codEmpleado, departamento;
-    private final int expResult;
+    private String codEmpleado, departamento;
+    private int expResult;
 
     private Empleado empleado;
 
-    public EmpleadoTestParametrizado(String nombre, long codEmpleado, String oficio, long departamento, int expResult){
+    public EmpleadoTestParametrizado(String nombre, String codEmpleado, String oficio, String departamento, int expResult){
 
         this.nombre = nombre;
         this.codEmpleado = codEmpleado;
@@ -30,8 +31,8 @@ public class EmpleadoTestParametrizado {
     public static List<Object[]> datosEmpleado(){
 
         return Arrays.asList(new Object[]
-                {"Pepe", 001, "Analista", 12, 2500}, new Object[] {"Luis", 031, "Programador", 01, 2000},
-                new Object[] {"Marisa", 144, "Diseñador", 55, 1500}, new Object[] {"Marcos", 002, "Programador", 66, 2000}
+                {"Pepe", "001", "Analista", "12", 2500}, new Object[] {"Luis", "031", "Programador", "01", 2000},
+                new Object[] {"Marisa", "144", "Diseñador", "55", 1500}, new Object[] {"Marcos", "002", "Programador", "66", 2000}
         );
     }
 
@@ -43,15 +44,15 @@ public class EmpleadoTestParametrizado {
         System.out.println("Comprobar Oficio");
 
         this.nombre = "";
-        this.codEmpleado = 0;
-        this.oficio = "";
-        this.departamento = 0;
-
-        empleado.comprobarOficio();
+        this.codEmpleado = "";
+        this.oficio = empleado.getOficio();
+        this.departamento = "";
+        this.expResult = 0;
 
         int salario = empleado.getSalario();
 
-        Assertions.assertEquals(this.expResult, salario);
+        Assertions.assertTrue((oficio.equals("Analista")) || (oficio.equals("Diseñador")) || (oficio.equals("Programador")));
+        Assertions.assertEquals(expResult, salario);
 
     }
 //
